0dbb0f2af380b0fc62ba810ba95c1f56da892287
ShaiKl <shay.kleiman@gmail.com>
2023-04-23 15:40:08 +0300

    test

--- test.asm
+++ test.asm
@@ -1,20 +1,16 @@
 ; Define constants
-section .data
+.data
 arr: .short 6, 0xEA, 0x22, 0x4B1D, 0b1010
 
 ; Define the program entry point and text segment
-section .text
-global _start
+.text
+.global _start
 
 _start:
 
 ; Exit system call number for x86-64
-mov rax, 60 ; sys_exit
-xor rdi, rdi ; exit status 0 (rdi = 0)
+mov %rax, $60 ; sys_exit
+xor %rdi, %rdi ; exit status 0 (rdi = 0)
 
 ; Perform the system call
 syscall
-
-; This part is not necessary, but you can keep it if you want
-xor rcx, rcx
-ret

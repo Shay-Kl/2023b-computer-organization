e5e0c2877bdf6d8a582b234e704797498a880d6a
ShaiKl <shay.kleiman@gmail.com>
2023-04-23 15:32:40 +0300

    test

---
+++ examp.asm
@@ -0,0 +1,27 @@
+.data
+a: .long 0
+b: .long 0
+inputFormat: .asciz "%d%d"
+outputFormat: .asciz "%d"
+ 
+.extern _printf
+.extern _scanf
+.text
+.global _main
+_main:
+_start:
+    movl %esp, %ebp # for correct debugging
+    pushl $b
+    pushl $a
+    pushl $inputFormat
+    call _scanf
+    movl a, %eax
+    addl b, %eax
+    pushl %eax
+    pushl $outputFormat
+    call _printf
+    movl %ebp, %esp
+    xorl %eax, %eax
+    ret
+    
+    

18053fca9b72c6ac90612d2dcfc54d8882711340
ShaiKl <shay.kleiman@gmail.com>
2023-04-23 15:40:55 +0300

    test

--- test.asm
+++ test.asm
@@ -1,16 +1,12 @@
-; Define constants
 .data
 arr: .short 6, 0xEA, 0x22, 0x4B1D, 0b1010
 
-; Define the program entry point and text segment
 .text
 .global _start
 
 _start:
 
-; Exit system call number for x86-64
-mov %rax, $60 ; sys_exit
-xor %rdi, %rdi ; exit status 0 (rdi = 0)
+mov %rax, $60 
+xor %rdi, %rdi 
 
-; Perform the system call
 syscall

fc9019adb9622d0e89aa7bd0473ddbbccaa3819d
ShaiKl <shay.kleiman@gmail.com>
2023-04-24 13:07:21 +0000

    ex1 fixed

--- HW1/wet/ex1.asm
+++ HW1/wet/ex1.asm
@@ -8,18 +8,15 @@ num: .quad 0xFFFFAAAA0000AAAA
 
 .section .text
 _start:
-	movb 64, %cl #cl = 63
+	movb $64, %cl #cl = 63
 	xorq %r9, %r9 #r9 = 0
 loop_HW1:
 	decb %cl #cl--
 	movq (num), %r8 # r8 = num
 	shlq %cl, %r8 #r8<<cl
-	shrq 63, %r8 #r8>>63
+	shrq $63, %r8 #r8>>63
 	addb %r8b, %r9b #r9b+=r8b
 	test %cl, %cl
 	jnz loop_HW1
 	movb %r9b, (bool)
-	
-	
-	
-	
+exit_HW1:

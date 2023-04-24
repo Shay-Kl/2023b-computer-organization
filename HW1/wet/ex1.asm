4a2aae735542c13e373930f82834fc022041ff28
ShaiKl <shay.kleiman@gmail.com>
2023-04-24 13:30:37 +0000

    ex1final

--- HW1/wet/ex1.asm
+++ HW1/wet/ex1.asm
@@ -9,14 +9,18 @@ num: .quad 0xFFFFAAAA0000AAAA
 .section .text
 _start:
 	movb $64, %cl #cl = 63
-	xorq %r9, %r9 #r9 = 0
-loop_HW1:
+	xorb %r9b, %r9b #r9 = 0
+	movq (num), %r10 #r10 = num
+loop_HW1: #do {
 	decb %cl #cl--
-	movq (num), %r8 # r8 = num
+	movq %r10, %r8 # r8 = num
 	shlq %cl, %r8 #r8<<cl
 	shrq $63, %r8 #r8>>63
 	addb %r8b, %r9b #r9b+=r8b
-	test %cl, %cl
-	jnz loop_HW1
-	movb %r9b, (bool)
+	testb %cl, %cl
+	jnz loop_HW1 #} while(cl!=0)	
+	movb %r9b, (bool) #bool = r9b
 exit_HW1:
+	
+	
+	

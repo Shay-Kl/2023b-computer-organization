047ed12d5f32063d12777b84e9dab5a2cd57a230
ShaiKl <shay.kleiman@gmail.com>
2023-04-24 15:55:40 +0300

    ex1

--- HW1/wet/ex1.asm
+++ HW1/wet/ex1.asm
@@ -1,5 +1,25 @@
 .global _start
 
+.section .data
+num: .quad 0xFFFFAAAA0000AAAA
+
+.section .bss
+.lcomm bool, 1
+
 .section .text
 _start:
-#your code here
+	movb 64, %cl #cl = 63
+	xorq %r9, %r9 #r9 = 0
+loop_HW1:
+	decb %cl #cl--
+	movq (num), %r8 # r8 = num
+	shlq %cl, %r8 #r8<<cl
+	shrq 63, %r8 #r8>>63
+	addb %r8b, %r9b #r9b+=r8b
+	test %cl, %cl
+	jnz loop_HW1
+	movb %r9b, (bool)
+	
+	
+	
+	

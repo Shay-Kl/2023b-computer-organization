f2eea33c35d4a5557a76bd68366ef4dd757fa8d8
ShaiKl <shay.kleiman@gmail.com>
2023-05-30 16:37:57 +0000

    fixed bug

--- 2_wet/part2/ili_handler.asm
+++ 2_wet/part2/ili_handler.asm
@@ -3,82 +3,90 @@
 .text
 .align 4, 0x90
 my_ili_handler:
-  pushq %rax
-  pushq %rdi
-  movq 16(%rsp), %rdi
-  pushq %rbx
-  pushq %rcx
-  pushq %rdx	
-  pushq %r8
-  pushq %r9
-  pushq %r10
-  pushq %r11
-  pushq %r12
-  pushq %r13
-  pushq %r14
-  pushq %r15
-  pushq %rsi
-  pushq %rbp
-  pushq %rsp
-  mov (%rdi), %rcx
-  cmpb $0x0f, %cl
-  je two_bytes
-  mov %rcx, %rdi
-  call what_to_do
-  jmp check_return
+	push %rax
+	push %rdi
+  	movq 16(%rsp), %rdi
+	push %rbx
+	push %rcx
+	push %rdx
+	push %rbp
+	push %rsi
+	push %r8
+	push %r9
+	push %r10
+	push %r11
+	push %r12
+	push %r13
+	push %r14
+	push %r15
+	mov (%rdi), %rcx
+	cmpb $0x0f, %cl
+	je two_bytes
+one_byte:
+	mov %rcx, %rdi
+	call what_to_do
+	
+  	test %rax, %rax
+  	jz normal_handler
+	pop %r15
+	pop %r14
+	pop %r13
+	pop %r12
+	pop %r11
+	pop %r10
+	pop %r9
+	pop %r8
+	pop %rsi
+	pop %rbp
+	pop %rdx
+	pop %rcx
+	pop %rbx
+	pop %rdi
+	mov %rax, %rdi
+	pop %rax
+	addq $1, (%rsp)
+	iretq
   
   
 two_bytes:
-  mov 1(%rdi), %rdi
-  call what_to_do
-  
-check_return:
-  cmp $0, %rax
-  jne unique_handler
+	mov 1(%rdi), %rdi
+  	call what_to_do
+  	test %rax, %rax
+  	jz normal_handler
+	pop %r15
+	pop %r14
+	pop %r13
+	pop %r12
+	pop %r11
+	pop %r10
+	pop %r9
+	pop %r8
+	pop %rsi
+	pop %rbp
+	pop %rdx
+	pop %rcx
+	pop %rbx
+	pop %rdi
+	mov %rax, %rdi
+	pop %rax
+	addq $2, (%rsp)
+	iretq
   
 normal_handler:
-  popq %rsp
-  popq %rbp
-  popq %rsi
-  popq %r15
-  popq %r14
-  popq %r13
-  popq %r12
-  popq %r11
-  popq %r10
-  popq %r9
-  popq %r8
-  popq %rdx
-  popq %rcx
-  popq %rbx
-  popq %rdi
-  popq %rax
-  jmp * old_ili_handler
-
-
-
-
-
-unique_handler:
-  popq %rsp
-  popq %rbp
-  popq %rsi
-  popq %r15
-  popq %r14
-  popq %r13
-  popq %r12
-  popq %r11
-  popq %r10
-  popq %r9
-  popq %r8
-  popq %rdx
-  popq %rcx
-  popq %rbx
-  popq %rdi
-  movq %rax, %rdi
-  popq %rax
-  
-  addq $2, (%rsp)
-
-  
-  iretq
+	pop %r15
+	pop %r14
+	pop %r13
+	pop %r12
+	pop %r11
+	pop %r10
+	pop %r9
+	pop %r8
+	pop %rsi
+	pop %rbp
+	pop %rdx
+	pop %rcx
+	pop %rbx
+	pop %rdi
+	pop %rax
+	jmp * old_ili_handler
+	

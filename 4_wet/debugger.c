abf5e199524351fa2cb20d57aea7c6ba14e26622
ShaiKl <shay.kleiman@gmail.com>
2023-07-01 13:36:40 +0300

    wer4 wip

---
+++ 4_wet/debugger.c
@@ -0,0 +1,13 @@
+#include "hw3_part1.c"
+
+int main(int argc, char *argv[])
+{
+    if (argc < 4)
+    {
+        printf("Not enough arguments");
+        return;
+    }
+    char* function = argv[1];
+    char* file = argv[2];
+    char* args[] = argv[2];
+}

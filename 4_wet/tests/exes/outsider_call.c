a01a09ef1528cac103487e062dbf624ef654102e
Shai <shay.kleiman@gmail.com>
2023-07-05 19:34:40 +0300

    complete?

---
+++ 4_wet/tests/exes/outsider_call.c
@@ -0,0 +1,18 @@
+
+#include "dynamic_utils.h"  
+#include <stdio.h>
+
+extern int func_to_track(int a, int b);
+extern int reqursive_func_to_track(int a, int b);
+
+int outsider_call_counter = 0;
+
+int outsider_call(int a)
+{
+    func_to_track(a, outsider_call_counter++);
+}
+
+int reqursive_outsider_call(int a)
+{
+    reqursive_func_to_track(a, outsider_call_counter++);
+}
